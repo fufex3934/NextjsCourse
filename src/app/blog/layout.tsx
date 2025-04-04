a50f713e88b1123127eb23b6f3f3410efc2fbd62
fufex3934 <fufawakgari174@gmail.com>
2025-04-04 13:35:20 +0300

    second commit

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function BlogLayout({
+    children,
+}:{children:React.ReactNode}) {
+  return (
+    <div>
+        <h1>Blog Layout</h1>
+        {children}
+    </div>
+  )
+}
