30c1a00cc57db35725bc014e7acf638d22bf636e
fufex3934 <fufawakgari174@gmail.com>
2025-04-05 10:35:48 +0300

    dynamic routes

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import type { Metadata } from 'next'
 
+export const metadata:Metadata = {
+    title:'My Blog',
+    description:"My blog description",
+}
 export default function BlogLayout({
     children,
-}:{children:React.ReactNode}) {
+    team,
+    analytics,
+  }: {
+    children: React.ReactNode
+    analytics: React.ReactNode
+    team: React.ReactNode
+  }) {
   return (
     <div>
         <h1>Blog Layout</h1>
         {children}
+        {team}
+        {analytics}
     </div>
   )
 }
